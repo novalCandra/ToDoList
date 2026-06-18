e50bf5a595bce1ea61266c1a560cef348bfad96d
novalCandra <dinatanovel5@gmail.com>
2026-06-18 15:18:03 +0700

    feat : ✅ sucess page profile

---
+++ profile.js
@@ -0,0 +1,8 @@
+
+// STATE PROFILE
+const listDropDownProfile = document.getElementById("listDropdownProfile");
+const buttonProfile = document.getElementById("buttonMenuProfile");
+// END PROFILE
+buttonProfile.addEventListener("click", () => {
+    listDropDownProfile.classList.toggle("block")
+})
